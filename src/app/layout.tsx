89fa19c6d0e1c873060056ba94f92d8884e42703
David Mellors <davidroymellors@gmail.com>
2024-07-12 09:26:09 +0200

    Make Mohara nav img a link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from 'geist/font/sans';
 import { TRPCReactProvider } from '~/trpc/react';
 import { ClerkProvider, SignedIn, SignedOut } from '@clerk/nextjs';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const metadata = {
   title: 'Course Website',
@@ -21,14 +22,14 @@ export default function RootLayout({
         <body className="h-full w-full p-0 m-0 flex flex-col">
           <header className="bg-black text-white p-4">
             <nav className="container mx-auto relative flex items-center justify-between">
-              <div className="p-3">
+              <Link href={'/'} className="p-3">
                 <Image
                   src={'/images/MoharaLogoWhite.png'}
                   alt="MoharaLogo"
                   width={30}
                   height={30}
                 />
-              </div>
+              </Link>
 
               <div className="absolute left-1/2 transform -translate-x-1/2">
                 <a href="/courses" className="text-xl font-bold">
